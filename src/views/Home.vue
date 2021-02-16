6d9484ad76d3c4b7adfbc4ada17dbe94a852356d
eleacarton <eleacarton@me.com>
2021-02-16 13:53:22 +0100

    conditions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
     <h1>{{ myTitle }}</h1>
-    <p>
-      J'ai {{ myAge }} ans
-    </p>
-    <p>
-      <!-- Conditions if ? else : -->
-      {{ myAge > 30 ? "Je suis vieux" : "Je suis jeune" }}
-    </p>
-    <p>
-      {{ percentCa / 100 }}
-    </p>
+
+    <p>J'ai {{ myAge }} ans</p>
+
+    <!-- Conditions if ? else : -->
+    <p>{{ myAge > 30 ? "Je suis vieux" : "Je suis jeune" }}</p>
+    
+    <p>{{ percentCa / 100 }}</p>
+
     <div class="unvisible__block" v-if="!isVisible">
       <p>je suis invisible</p>
     </div>
-    <div class="visible__block" v-else>
+    <div class="visible__block" v-else-if="isVisible">
       <p>je suis visible</p>
     </div>
+    <div class="visible__block" v-else>
+      <p>je suis ni visible ni invisible</p>
+    </div>
+
+    <div class="font-bold" v-html="textStyling">
+      <p>{{ textStyling }}</p>
+    </div>
+
   </div>
 </template>
 
@@ -31,7 +37,8 @@ export default {
       myTitle: "Ma homepage",
       myAge: 30,
       percentCa: 2000,
-      isVisible: false
+      isVisible: false,
+      textStyling: "<strong>This text is prout</strong>"
     }
   }
 }
