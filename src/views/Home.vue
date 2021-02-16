8c8effdee5b195268daa45a2b7077e0db76e349b
eleacarton <eleacarton@me.com>
2021-02-16 14:32:15 +0100

    add for and click

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,6 +23,25 @@
       <p>{{ textStyling }}</p>
     </div>
 
+    <div class="google__logo">
+      <img :src="googleLogo" alt="">
+    </div>
+
+    <div class="links">
+      <a target="_blank" :href="link">Link to google</a> <br>
+      <a target="_blank" v-bind:href="link">Link to google</a>
+    </div>
+    
+    <div class="products__grid" v-for="(product, index) in productsArray" v-bind:key="product.id">
+      {{ index }}
+      {{ product.id }}
+      {{ product.title }}
+      {{ product.price }} €
+    </div>
+
+    <button v-on:click="sayHello">say Hello</button>
+    <button @click="sayHello">say Hello</button>
+
   </div>
 </template>
 
@@ -32,13 +51,24 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  methods: {
+    sayHello: function() {
+      alert('Hello');
+    }
+  },
   data: function() {
     return {
       myTitle: "Ma homepage",
       myAge: 30,
       percentCa: 2000,
       isVisible: false,
-      textStyling: "<strong>This text is prout</strong>"
+      textStyling: "<strong>This text is prout</strong>",
+      link: "https://google.fr",
+      googleLogo: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+      productsArray: [
+        { id:1, title:"product 1", price: 25 },
+        { id:2, title:"product 2", price: 21 }
+      ]
     }
   }
 }
