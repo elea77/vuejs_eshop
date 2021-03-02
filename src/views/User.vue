f5a49ab1f04a59fa70a838fac25d444e870b441d
eleacarton <eleacarton@me.com>
2021-03-02 13:39:28 +0100

    add shop and user pages

---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <TitlePage title="Mon compte"/>
+    </div>
+</template>
+
+<script>
+    import TitlePage from "../components/TitlePage";
+
+    export default {
+        components: {
+            TitlePage
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
