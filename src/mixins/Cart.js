5f154e69aabfc06f319bd326dcaa47aab3b5f92c
eleacarton <eleacarton@me.com>
2021-03-16 18:00:30 +0100

    update

--- src/mixins/Cart.js
+++ src/mixins/Cart.js
@@ -7,17 +7,25 @@ export default {
         addToCart(product) {
 
             let cart = JSON.parse(localStorage.getItem('cart')) || [];
-            console.log(product);
+
             let productObject = {
                 id: product._id,
                 title: product.title,
                 price: product.price,
                 qty: 1
             }
+            console.log(cart);
+            console.log(cart.includes(product._id));
+            // if(cart)
+            let indexOfExistingProduct = cart.findIndex(
+                (el) => el.id === productObject.id
+            )
 
-            //findIndex => renvoies l'index ou -1
-
-            cart.push(productObject);
+            if(indexOfExistingProduct !== -1) {
+                cart[indexOfExistingProduct].qty += 1
+            } else {
+                cart.push(productObject);
+            }
 
             localStorage.setItem('cart', JSON.stringify(cart));
 
@@ -29,15 +37,33 @@ export default {
             // Récupérer le panier localStorage.getItem + parser
             // Le modifier
             // le réinsérer localStorage.setItem
+
+            let card = JSON.parse(localStorage.getItem('cart'));
+            const filterCard = card.filter((item) => {
+
+            });
+            localStorage.setItem('cart', JSON.stringify(filteredCard));
         },
         clearCart() {
             //localStorage.removeItem('cart');
+            localStorage.removeItem('cart');
         },
         removeOneQty(product) {
 
         },
         addOneQty(product) {
 
+        },
+        getCartTotal() {
+            // let cart = JSON.parse(localStorage.getItem('cart'));
+            
+            // const reducer = (price, qty) => price * qty;
+
+            // console.log(cart.reduce(reducer));
+
+        },
+        getCartCount() {
+            //Array.Reduce
         }
     }
 }
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,12 +1,53 @@
 <template>
     <div class="shopping__cart">
+        <TitlePage title="Panier"/>
+        <table>
+            <thead>
+                <tr>
+                    <th>Titre</th>
+                    <th>Quantité</th>
+                    <th>Prix</th>
+                    <th></th>
+                    <th>Total</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in cartArray" v-bind:key="item._id">
+                    <td>{{item.title}}</td>
+                    <td>{{item.qty}}</td>
+                    <td>{{item.price}} €</td>
+                    <td>
+                        <button>-</button>
+                        <button>+</button>
+                    </td>
+                    <td>
+                        {{ item.qty * item.price }} €
+                    </td>
+                    <td>
+                        <button>Supprimer du panier</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table> <br><br>
+        <button @click="deleteCart()">Supprimer le panier</button>
 
     </div>
 </template>
 
 <script>
 import Cart from '../mixins/Cart'
+import TitlePage from "../components/TitlePage";
+
     export default {
+        components: {
+            TitlePage
+        },
+        methods: {
+            deleteCart: function() {
+                this.clearCart();
+            }
+        },
         mixins: [Cart],
         data: function() {
             return {
@@ -20,5 +61,7 @@ import Cart from '../mixins/Cart'
 </script>
 
 <style lang="scss" scoped>
-
+    table {
+        margin: auto;
+    }
 </style>
